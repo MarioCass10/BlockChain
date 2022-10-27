ec30bcf34da01bc1acd14804b3b96e02733958ff
Mario <mario.castillo.ceb@gmail.com>
2022-10-27 09:40:30 +0200

    Modificaciones en el Qa3

--- Project1/Q3a.py
+++ Project1/Q3a.py
@@ -9,13 +9,13 @@ from Q1 import send_from_P2PKH_transaction
 
 
 cust1_private_key = CBitcoinSecret(
-    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
+    'Kz9PPTCxqkvMXtf51t3u88ueGGU7UGZbfNXHHeuZ2d9vd9fWGkTD')
 cust1_public_key = cust1_private_key.pub
 cust2_private_key = CBitcoinSecret(
-    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
+    'L3sB23pe1Gmq3RULvHGsQbeVRz1mignarYmdabFFaB2nVPtm1Zt7')
 cust2_public_key = cust2_private_key.pub
 cust3_private_key = CBitcoinSecret(
-    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
+    'KysibXCzvR8Yy2t31sVFP3KvqjqdBzAyFJdeeyTQKh79dSueHdEx')
 cust3_public_key = cust3_private_key.pub
 
 
@@ -27,17 +27,17 @@ cust3_public_key = cust3_private_key.pub
 # bank_private_key.
 
 Q3a_txout_scriptPubKey = [
-        # fill this in!
+        OP_2, cust1_public_key, cust2_public_key,cust3_public_key, my_public_key, OP_4, OP_EQUALVERIFY# fill this in!
 ]
 ######################################################################
 
 if __name__ == '__main__':
     ######################################################################
     # TODO: set these parameters correctly
-    amount_to_send = None # amount of BTC in the output you're sending minus fee
+    amount_to_send = 0.00103403 - 0.0003 # amount of BTC in the output you're sending minus fee
     txid_to_spend = (
-        'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
-    utxo_index = None # index of the output you are spending, indices start at 0
+        'ce51d2ca0a5d1d22e81aa9f6c569dc5ae755dc548528492472d3a278b53d7e20')
+    utxo_index = 3 # index of the output you are spending, indices start at 0
     ######################################################################
 
     response = send_from_P2PKH_transaction(amount_to_send, txid_to_spend, 
